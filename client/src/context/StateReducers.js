c110c7a967f57fa5461a3b0ea2a96dcbd401a806
Rafael Herrera <rafael.herrera@neotek.mx>
2023-09-10 20:00:15 -0600

    Add a list of chats with pending status messages

--- client/src/context/StateReducers.js
+++ client/src/context/StateReducers.js
@@ -7,7 +7,9 @@ export const initialState = {
     currentChatUser: undefined,
     messages: [],
     socket: undefined,
-    messagesSearch: false
+    messagesSearch: false,
+    userContacts: [],
+    onlineUsers: []
 }
 
 const reducer = (state, action) => {
@@ -19,54 +21,59 @@ const reducer = (state, action) => {
                 ...state,
                 userInfo: action.userInfo
             }
-
         case reducerCases.SET_NEW_USER:
             
             return {
                 ...state,
                 newUser: action.newUser
             }
-
         case reducerCases.SET_ALL_CONTACTS_PAGE:
 
             return {
                 ...state,
                 contactsPage: !state.contactsPage
             }
-
         case reducerCases.CHANGE_CURRENT_CHAT_USER:
 
             return {
                 ...state,
                 currentChatUser: action.user,
             }
-        
         case reducerCases.SET_MESSAGES:
 
             return {
                 ...state,
                 messages: action.messages
             }
-
         case reducerCases.SET_SOCKET:
 
             return {
                 ...state,
                 socket: action.socket
             }
-
         case reducerCases.ADD_MESSAGE:
 
             return {
                 ...state,
                 messages: [...state.messages, action.newMessage]
             }
-            
         case reducerCases.SET_MESSAGE_SEARCH:
 
             return {
                 ...state,
                 messagesSearch: !state.messagesSearch
+            }
+        case reducerCases.SET_USER_CONTACTS:
+
+            return {
+                ...state,
+                userContacts: action.userContacts
+            }
+        case reducerCases.SET_ONLINE_USERS:
+
+            return {
+                ...state,
+                onlineUsers: action.onlineUsers
             }
         default:
             return state
--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -204,3 +204,118 @@ export const addAudioMessage = async (req, res, next) => {
         next(err)
     }
 }
+
+export const getInitialContactsWithMessages = async (req, res, next) => {
+
+    try {
+        
+        const userId = parseInt(req.params.from)
+        const prisma = getPrismaInstance()
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            include: {
+                sentMessages: {
+                    include: {
+                        reciever: true,
+                        sender: true
+                    },
+                    orderBy: {
+                        createdAt: 'desc',
+                    },
+                },
+                recievedMessages: {
+                    include: {
+                        reciever: true,
+                        sender: true
+                    },
+                    orderBy: {
+                        createdAt: 'desc',
+                    },
+                },
+            }
+        })
+
+        const messages = [...user.sentMessages, ...user.recievedMessages]
+
+        messages.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+
+        const users = new Map()
+        const messagesStatusChange = []
+
+        messages.forEach(msg => {
+            
+            const isSender = msg.senderId === userId
+            const calculatedId = isSender ? msg.recieverId : msg.senderId
+
+            if (msg.messageStatus === 'sent') {
+                
+                messagesStatusChange.push(msg.id)
+            }
+
+            const {id, type, message, messageStatus, createdAt, senderId, recieverId} = msg
+
+            if (!users.get(calculatedId)) {
+                
+                let user = {
+                    messageId: id,
+                    type,
+                    message,
+                    messageStatus,
+                    createdAt,
+                    senderId,
+                    recieverId,
+                }
+
+                if (isSender) {
+                    
+                    user = {
+                        ...user,
+                        ...msg.reciever,
+                        totalUnreadMessages: 0,
+                    }
+                }else{
+
+                    user = {
+                        ...user,
+                        ...msg.sender,
+                        totalUnreadMessages: messageStatus !== 'read'? 1 : 0,
+                    }
+                }
+
+                users.set(calculatedId, { ...user })
+            }else if(messageStatus !== 'read' && !isSender){
+
+                const user = users.get(calculatedId) 
+
+                users.set(calculatedId, {
+                    ...user,
+                    totalUnreadMessages: user.totalUnreadMessages+1,
+                })
+            }
+        })
+
+        if (messagesStatusChange.length) {
+            
+            await prisma.messages.updateMany({
+                where: {
+                    id: {
+                        in: messagesStatusChange
+                    }
+                },
+                data: {
+                    messageStatus: 'delivered'
+                }
+            })
+        }
+
+        return res.status(200).json({
+            users: Array.from(users.values()),
+            onlineUsers: Array.from(onlineUsers.keys())
+        })
+    } catch (error) {
+
+        next(error)
+    }
+}
